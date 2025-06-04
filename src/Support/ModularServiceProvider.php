5b132950dbb2a290448c6cb779fd9110c1dd8f7f
Tristan <t.chanove@myanapro.com>
2025-06-04 15:24:29 +0200

    feat: add module service provider auto discovery

--- src/Support/ModularServiceProvider.php
+++ src/Support/ModularServiceProvider.php
@@ -70,6 +70,12 @@ class ModularServiceProvider extends ServiceProvider
 			return new MigrateMakeCommand($app['migration.creator'], $app['composer']);
 		});
 
+		$this->app->singleton(\Illuminate\Foundation\PackageManifest::class, fn() => new PackageManifest(
+			new Filesystem(),
+			$this->app->basePath(),
+			$this->app->getCachedPackagesPath()
+		));
+
 		$this->registerEloquentFactories();
 
 		$this->app->resolving(Migrator::class, fn(Migrator $migrator) => $this->autodiscover()->migrations($migrator));
---
+++ tests/Commands/PackageManifestTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Commands;
+
+use Illuminate\Foundation\PackageManifest;
+use InterNACHI\Modular\Tests\TestCase;
+
+class PackageManifestTest extends TestCase
+{
+	public function test_it_overrides_package_manifest_providers_discovery(): void
+	{
+		$this->makeModule('test-module-1');
+		$this->makeModule('test-module-2');
+
+		$this->app->make(PackageManifest::class)->build();
+
+		$discoveredProviders = $this->app->make(PackageManifest::class)->providers();
+
+		self::assertContains('Modules\TestModule1\Providers\TestModule1ServiceProvider', $discoveredProviders);
+		self::assertContains('Modules\TestModule2\Providers\TestModule2ServiceProvider', $discoveredProviders);
+	}
+}
